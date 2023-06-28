5d478d01b82613d3b38ebb0d9cfc6ee85c469ae7
Eitan Traurig <etraurig@mail.yu.edu>
2023-06-28 17:29:41 +0000

    Clean up code and files

--- selective_search.py
+++ selective_search.py
@@ -1,13 +1,10 @@
 # import the necessary packages
-import argparse
-import random
-import time
 import cv2
 
 
 def selective_search_fast(path):
 
-# load the input image
+	# load the input image
 	image = cv2.imread(path)
 	# initialize OpenCV's selective search implementation and set the
 	# input image
@@ -18,14 +15,12 @@ def selective_search_fast(path):
 
 	
 	# run selective search on the input image
-	start = time.time()
 	rects = ss.process()
-	end = time.time()
 	return rects
 
 def selective_search_quality(path):
 
-# load the input image
+	# load the input image
 	image = cv2.imread(path)
 	# initialize OpenCV's selective search implementation and set the
 	# input image
@@ -36,10 +31,5 @@ def selective_search_quality(path):
 
 	
 	# run selective search on the input image
-	start = time.time()
 	rects = ss.process()
-	end = time.time()
 	return rects
-
-
-selective_search_fast("orange.jpg")
