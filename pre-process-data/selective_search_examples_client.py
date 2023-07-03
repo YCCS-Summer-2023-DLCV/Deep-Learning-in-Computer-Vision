0845df8b01d7f499183f6c819b7b465a7073970f
TuvyaMacklin <tuvyamacklin@gmail.com>
2023-07-03 20:48:34 +0000

    added selective search dataset client code

--- pre-process-data/selective_search_examples_client.py
+++ pre-process-data/selective_search_examples_client.py
@@ -1,6 +1,7 @@
 import tensorflow_datasets as tfds
+from tqdm import tqdm
 
-from generate_selective_search_examples import generate_selective_search_examples
+from generate_selective_search_examples import generate_selective_search_examples, _get_label_ids
 
 ds_train, info = tfds.load("coco/2017", split = "train", with_info = True)
 all_labels = info.features["objects"]["label"].names
@@ -8,5 +9,5 @@ ds_val = tfds.load("coco/2017", split = "validation")
 
 labels = ["banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake"]
 
-generate_selective_search_examples(ds_train.take(20), labels, all_labels, "foods-ss", "train")
-#generate_selective_search_examples(ds_val, labels, all_labels, "foods-ss", "validation")
+generate_selective_search_examples(ds_train, labels, all_labels, "foods-ss", "train")
+generate_selective_search_examples(ds_val, labels, all_labels, "foods-ss", "validation")
