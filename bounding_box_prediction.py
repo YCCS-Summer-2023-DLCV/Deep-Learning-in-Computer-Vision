2e480cd197f1cfcf1eeec6f5ef76296d7445c3ae
Eitan Traurig <etraurig@mail.yu.edu>
2023-07-11 13:41:38 +0000

    Working non max suppression.

--- bounding_box_prediction.py
+++ bounding_box_prediction.py
@@ -14,7 +14,9 @@ def predict_image(image_path, model_path,max_bboxes):
     # Show the model architecture
     new_model.summary()
     image_bbox = selective_search.selective_search_fast(image_path)
-    labels = []
+    labels = [11][0]
+    sorted_bbox = [11][0]
+    test = np.empty(shape =(11,1))
     index =0 
     for i in image_bbox:
         cropped_image = _crop_image(img,i)
@@ -23,17 +25,26 @@ def predict_image(image_path, model_path,max_bboxes):
         cropped_image = tf.reshape(cropped_image, [1, 120, 120, 3])
         prediction = new_model.predict(cropped_image)
         prediction_max = np.max(prediction[0])
-      
-        labels.append(prediction_max)
+        test[3].append(prediction_max)
+        labels[np.argmax(prediction[0])].append(prediction_max)
         image_bbox[index] = (i[1],i[0],i[1]+i[3],i[0]+i[2])
+        sorted_bbox[np.argmax(prediction[0])].append(image_bbox[index])
         index += 1
        
-    return _non_max_suppersion(image_bbox, np.asarray(labels), max_bboxes)
+    return _non_max_suppersion(np.array(sorted_bbox), np.asarray(labels), max_bboxes)
 
 def _crop_image(example, bbox):
     bbox_rect = (bbox[0],bbox[1],bbox[0]+bbox[2],bbox[1]+bbox[3])
     return example.crop(bbox_rect)
 
 def _non_max_suppersion(boxes, scores, max_bboxes):
-    selected_indices = tf.image.non_max_suppression(boxes,scores,max_bboxes,iou_threshold=0.5)
+    
+    selected_indices = tf.image.non_max_suppression(boxes[3],scores[3],max_bboxes,iou_threshold=0.3)
     return tf.gather(boxes, selected_indices)
+
+box = predict_image("vegetables.jpg","model/saved_model/efficientnet_b1",1)
+image = cv2.imread("vegetables.jpg")
+boxes = box.numpy()
+for (startY, startX, endY, endX) in boxes:
+    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 1)
+cv2.imwrite("vegetables_one.jpg", image)
