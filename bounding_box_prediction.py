e5c22f174b11dee763b549520743d049fc1000f5
Eitan Traurig <etraurig@mail.yu.edu>
2023-07-11 18:12:30 +0000

    Add option to predict without non max suppression

--- bounding_box_prediction.py
+++ bounding_box_prediction.py
@@ -5,19 +5,22 @@ import selective_search
 from PIL import Image
 import cv2
 
-def predict_image(image_path, model_path,max_bboxes):
+labels = ["apple","background", "banana",  "broccoli" ,  "cake", "carrot", "donut", "hot dog", "orange", "pizza", "sandwich"]
+
+def _predict_image(image_path, model_path):
  
- # Recreate the exact same model, including its weights and the optimizer
+    # Recreate the exact same model, including its weights and the optimizer
     img = Image.open(image_path)
     new_model = tf.keras.models.load_model(model_path)
 
     # Show the model architecture
     new_model.summary()
     image_bbox = selective_search.selective_search_fast(image_path)
-    labels = [11][0]
-    sorted_bbox = [11][0]
-    test = np.empty(shape =(11,1))
-    index =0 
+    scores = dict()
+    sorted_bbox = dict()
+    for i in range(0,11):
+        scores[i] = list()
+        sorted_bbox[i] = list()
     for i in image_bbox:
         cropped_image = _crop_image(img,i)
         cropped_image  = tf.keras.utils.img_to_array(cropped_image)
@@ -25,26 +28,55 @@ def predict_image(image_path, model_path,max_bboxes):
         cropped_image = tf.reshape(cropped_image, [1, 120, 120, 3])
         prediction = new_model.predict(cropped_image)
         prediction_max = np.max(prediction[0])
-        test[3].append(prediction_max)
-        labels[np.argmax(prediction[0])].append(prediction_max)
-        image_bbox[index] = (i[1],i[0],i[1]+i[3],i[0]+i[2])
-        sorted_bbox[np.argmax(prediction[0])].append(image_bbox[index])
-        index += 1
+        test = np.argmax(prediction[0])
+        scores[test].append(prediction_max)
+        sorted_bbox[test] = np.append(sorted_bbox[test], (i[1],i[0],i[1]+i[3],i[0]+i[2]), axis=0)
        
-    return _non_max_suppersion(np.array(sorted_bbox), np.asarray(labels), max_bboxes)
+    return sorted_bbox,scores
 
 def _crop_image(example, bbox):
     bbox_rect = (bbox[0],bbox[1],bbox[0]+bbox[2],bbox[1]+bbox[3])
     return example.crop(bbox_rect)
 
 def _non_max_suppersion(boxes, scores, max_bboxes):
-    
-    selected_indices = tf.image.non_max_suppression(boxes[3],scores[3],max_bboxes,iou_threshold=0.3)
-    return tf.gather(boxes, selected_indices)
+    nms_boxes = dict()    
+    for i in range(0,11):
+        length = len(scores[i])
+        if i in boxes and not i == 1 :
+            box = np.asarray(boxes[i], dtype=np.int32).reshape(length,4)
+            score = np.asarray(scores[i])
+            selected_indices = tf.image.non_max_suppression(box, score,max_bboxes,iou_threshold=0.3, score_threshold = 11.0)
+            nms_boxes[i] = tf.gather(box, selected_indices).numpy()
+    return nms_boxes
 
-box = predict_image("vegetables.jpg","model/saved_model/efficientnet_b1",1)
-image = cv2.imread("vegetables.jpg")
-boxes = box.numpy()
-for (startY, startX, endY, endX) in boxes:
-    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 1)
-cv2.imwrite("vegetables_one.jpg", image)
+def _draw_bbox(boxes, image_path, final_path):
+    image = cv2.imread(image_path)
+    for i in range (0,11):
+        if i in boxes:
+            for (startY, startX, endY, endX) in boxes[i]:
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 1)
+                (w, h), _ = cv2.getTextSize(
+                    labels[i], cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+
+                image = cv2.rectangle(image, (startX, startY - 20), (startX + w, startY), (0, 0, 255), -1)
+                image = cv2.putText(image, labels[i], (startX, startY - 5),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 1)
+        
+    cv2.imwrite(final_path, image)
+
+def predict_image_with_nms(image_path,final_path,  model_path, max_bboxes):
+    sorted_bbox,scores = _predict_image(image_path,model_path)
+    box = _non_max_suppersion(sorted_bbox,scores,max_bboxes)
+    _draw_bbox(box,image_path,final_path)
+
+def predict_image_without_nms(image_path,final_path,  model_path):
+    sorted_bbox,scores, max_bboxes  = _predict_image(image_path,model_path)
+    boxes= dict()
+    for i in range (0,11):
+        if i in sorted_bbox and not i == 1:
+            length = len(scores[i])
+            boxes[i] = np.asarray(sorted_bbox[i], dtype=np.int32).reshape(length,4)
+    _draw_bbox(sorted_bbox,image_path,final_path)
+
+predict_image_with_nms("COCO_test2014_000000003901.jpg", "coco_test.jpg","model/saved_model/efficientnet_b1", 20)
+   
