a1df56cd8b167d8aa42dff2fbdaa9746f084969b
JasonZelmanovich <Jzelmano@mail.yu.edu>
2023-07-12 01:11:33 +0000

    Fixed confusion_matrix script, added ConMat 7.11

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,60 +1,56 @@
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 import tensorflow as tf
 from tensorflow import keras
-
-
+import seaborn as sns
+from sklearn.utils.multiclass import unique_labels
+import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
 # Assuming you have a pre-existing model called 'model'
+model = tf.keras.models.load_model("saved_model/efficientnet_b1")
 
 # Load the dataset
 BATCH_SIZE = 32
 IMG_SIZE = (120, 120)
 test_dir = "validation"
 
-train_ds = keras.utils.image_dataset_from_directory(
-    test_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=IMG_SIZE,
-    batch_size=BATCH_SIZE
-)
+test_generator = ImageDataGenerator()
+test_data_generator = test_generator.flow_from_directory(
+    test_dir, # Put your path here
+     target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    shuffle=False)
+test_steps_per_epoch = numpy.math.ceil(test_data_generator.samples / test_data_generator.batch_size)
 
-model = keras.models.load_model("saved_model/efficientnet_b1_2")
+predictions = model.predict_generator(test_data_generator, steps=test_steps_per_epoch)
+# Get most likely class
+predicted_classes = numpy.argmax(predictions, axis=1)
 
-# Retrieve the true labels from the dataset
-true_labels = []
-for images, labels in train_ds:
-    true_labels.extend(labels.numpy())
+true_classes = test_data_generator.classes
+class_labels = list(test_data_generator.class_indices.keys())   
 
-true_labels = np.array(true_labels)
+report = classification_report(true_classes, predicted_classes, target_names=class_labels)
+print(report) 
 
-# Generate predictions using the model
-predictions = model.predict(train_ds)
-predicted_labels = np.argmax(predictions, axis=1)
+cm = confusion_matrix(true_classes, predicted_classes)
+print(cm)
 
-# Create the confusion matrix
-confusion_mat = confusion_matrix(true_labels, predicted_labels)
+# Plot confusion matrix in a beautiful manner
+fig = plt.figure(figsize=(16, 14))
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, ax = ax, fmt = 'g'); #annot=True to annotate cells
+# labels, title and ticks
+ax.set_xlabel('Predicted', fontsize=20)
+ax.xaxis.set_label_position('bottom')
+plt.xticks(rotation=90)
+ax.xaxis.set_ticklabels(class_labels, fontsize = 10)
+ax.xaxis.tick_bottom()
 
-# Display the confusion matrix
-class_names = train_ds.class_names
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.imshow(confusion_mat, cmap='Blues')
-ax.set_xticks(np.arange(len(class_names)))
-ax.set_yticks(np.arange(len(class_names)))
-ax.set_xticklabels(class_names)
-ax.set_yticklabels(class_names)
+ax.set_ylabel('True', fontsize=20)
+ax.yaxis.set_ticklabels(class_labels, fontsize = 10)
+plt.yticks(rotation=0)
 
-# Rotate the x-axis labels for better visibility
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+plt.title('Refined Confusion Matrix', fontsize=20)
 
-# Loop over data dimensions and create text annotations
-for i in range(len(class_names)):
-    for j in range(len(class_names)):
-        ax.text(j, i, confusion_mat[i, j], ha="center", va="center", color="white")
-
-ax.set_title("Confusion Matrix")
-plt.xlabel("Predicted Labels")
-plt.ylabel("True Labels")
-plt.savefig("Confusion_efficientnetb1.jpeg")
+plt.savefig('ConMat24.png')
