fb35cf3525fe58a9d01da7966e3158c4d07f3884
Eitan Traurig <123205223+EitanSomethingYU@users.noreply.github.com>
2023-08-08 13:03:11 -0400

    Broccoli segmentation (#31)
    * test images
    
    * Add photos
    
    * higher learning rate
    
    * training with batch size of 64
    
    * created a segmentation dataset with only brocolli examples
    
    * "Making progress on segmentation"
    
    * "Revisions"
    
    * "More augmentation model"
    
    * generated a new dataset with a minimum size for the images
    
    * set up function to load a multiclass dataset
    
    * predict image tests
    
    * Remove images and fix drawing error
    
    * remove line
    
    * fix spacing
    
    * remove model
    
    * remove image
    
    * remove testing
    
    ---------
    
    Co-authored-by: TuvyaMacklin <tuvyamacklin@gmail.com>

--- inference/bounding_box_prediction.py
+++ inference/bounding_box_prediction.py
@@ -4,9 +4,10 @@ import tensorflow as tf
 import selective_search
 from PIL import Image
 import cv2
+import tensorflow_addons as tfa
 
 labels = ["apple","background", "banana",  "broccoli" ,  "cake", "carrot", "donut", "hot dog", "orange", "pizza", "sandwich"]
-
+labels_segmentation = ["broccoli"]
 def _predict_image(image_path, model_path):
  
     # Recreate the exact same model, including its weights and the optimizer
@@ -49,22 +50,80 @@ def _non_max_suppression(boxes, scores, max_bboxes, iou_threshold, score_thresho
             nms_boxes[i] = tf.gather(box, selected_indices).numpy()
     return nms_boxes
 
-def _draw_bbox(boxes, image_path, final_path):
+def _draw_bbox_with_segmentation(boxes, image_path, final_path, segmentation_model_path):
     image = cv2.imread(image_path)
+    output = image.copy()
+
     for i in range (0,11):
         if i in boxes:
             for (startY, startX, endY, endX) in boxes[i]:
-                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 1)
+                if labels[i] in labels_segmentation:
+                    _segmentaion(image_path,output,segmentation_model_path,startY,startX,endY,endX)     
+                cv2.rectangle(output, (startX, startY), (endX, endY), (0, 0, 255), 1)
                 (w, h), _ = cv2.getTextSize(
                     labels[i], cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
 
-                image = cv2.rectangle(image, (startX, startY - 20), (startX + w, startY), (0, 0, 255), -1)
-                image = cv2.putText(image, labels[i], (startX, startY - 5),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 1)
-        
-    cv2.imwrite(final_path, image)
+                output = cv2.rectangle(output, (startX, startY), (startX + w, startY +20), (0, 0, 255), -1)
+                output = cv2.putText(output, labels[i], (startX, startY + 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 1)                   
+                 
+    cv2.imwrite(final_path, output)
 
-def predict_image_with_nms(image_path,final_path,  model_path, max_bboxes, iou_threshold, score_threshold):
+
+def _draw_bbox(boxes, image_path, final_path):
+    image = cv2.imread(image_path)
+    output = image.copy()
+
+    for i in range (0,11):
+        if i in boxes:
+            for (startY, startX, endY, endX) in boxes[i]: 
+                cv2.rectangle(output, (startX, startY), (endX, endY), (0, 0, 255), 1)
+                (w, h), _ = cv2.getTextSize(
+                    labels[i], cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+
+                output = cv2.rectangle(output, (startX, startY), (startX + w, startY +20), (0, 0, 255), -1)
+                output = cv2.putText(output, labels[i], (startX, startY + 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 1)                   
+                 
+    cv2.imwrite(final_path, output)
+def _create_mask(pred_mask):
+  pred_mask = tf.math.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+  return pred_mask[0]
+
+def _segmentaion(image_path,output,model_path,startY,startX,endY,endX):
+    image = Image.open(image_path)
+    bbox_rect = (startX,startY,endX,endY)
+    cropped_image = image.crop(bbox_rect)
+    model = tf.keras.models.load_model(model_path)
+    cropped_image  = tf.keras.utils.img_to_array(cropped_image)
+    cropped_image = tf.image.resize(cropped_image,(128,128))
+    cropped_image_resize = tf.reshape(cropped_image, [1, 128, 128, 3])
+    mask_tensorflow = _create_mask(model.predict(cropped_image_resize))
+    mask = mask_tensorflow.numpy().astype("uint8")
+    mask = cv2.resize(mask, (endX-startX, endY-startY), interpolation=cv2.INTER_NEAREST)
+    mask[mask==0] = 2
+    mask[mask==1] = 0
+    mask[mask==2] = 1
+
+    color = np.array([255, 0, 0], dtype='uint8')
+    roi = output[startY:endY, startX:endX]
+    # randomly select a color that will be used to visualize this
+    # particular instance segmentation then create a transparent
+    # overlay by blending the randomly selected color with the ROI
+    stacked_mask = np.stack((mask,)*3, axis=-1)
+    masked_img  = np.where(stacked_mask, roi, color)
+    blended = cv2.addWeighted(roi, 0.8, masked_img, 0.2,0)
+
+    output[startY:endY, startX:endX] = blended
+
+def predict_image_with_nms_with_segmentation(image_path,final_path,  model_path, max_bboxes, iou_threshold, score_threshold, segmentation_model_path):
+    sorted_bbox,scores = _predict_image(image_path,model_path)
+    box = _non_max_suppression(sorted_bbox,scores,max_bboxes, iou_threshold,score_threshold)
+    _draw_bbox_with_segmentation(box,image_path,final_path, segmentation_model_path)
+
+
+def predict_image_with_nms(image_path,final_path,  model_path, max_bboxes, iou_threshold, score_threshold, segmentation_model_path):
     sorted_bbox,scores = _predict_image(image_path,model_path)
     box = _non_max_suppression(sorted_bbox,scores,max_bboxes, iou_threshold,score_threshold)
     _draw_bbox(box,image_path,final_path)
@@ -73,7 +132,7 @@ def predict_image_without_nms(image_path,final_path,  model_path):
     sorted_bbox,scores  = _predict_image(image_path,model_path)
     boxes= dict()
     for i in range (0,11):
-        if i in sorted_bbox and not i == 1:
+        if i in sorted_bbox and not i == 1 and not i==0:
             length = len(scores[i])
             boxes[i] = np.asarray(sorted_bbox[i], dtype=np.int32).reshape(length,4)
-    _draw_bbox(sorted_bbox,image_path,final_path)
+    _draw_bbox(boxes,image_path,final_path)
